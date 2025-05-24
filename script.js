bfd1c3df5fbc79308876212cde33f6e5f06ab6f2
Hiroe-Mizuno <hiroe_naruse@mail.toyota.co.jp>
2025-05-24 16:48:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,35 @@
 'use strict'
 
-// loading画面のアニメーションを消す指示
-let loadingScreen = document.getElementById("loading");
-
-document.addEventListener("DOMContentLoaded", function() {
-  setTimeout(function() {
-    if (loadingScreen) {
-      loadingScreen.remove();
+// ローディング画面を削除する
+  let loadingScreen = document.getElementById("loading");
+  setTimeout(deleteLodingScreen, 4900);
+    function deleteLodingScreen() {
+      if (loadingScreen) {
+        loadingScreen.remove();
+      }
     }
-  }, 4900); // 4.9秒後に削除する
-});
 
-// aboutMeボタンがクリックされた時の指示
-let aboutMeButton = document.getElementById("aboutMe"); 
-let greeting = ["Welcome!", "Thank you!", "Keep going!", "Fight!!"];
+// ボタン情報の取得、#greetingのテキストを準備
+  let aboutMeButton = document.getElementById("aboutMe"); 
+  let qaButton = document.getElementById("QA"); 
+  const greeting = ["Welcome!", "Thank you!", "Keep going!", "Fight!!"];    
 
-document.addEventListener("DOMContentLoaded", function() {
+// aboutMeボタンがクリックされたらアラート表示
   if(aboutMeButton) {
-    aboutMeButton.addEventListener("click", function() {
-    alert("40代・プログラミング歴１ヶ月の Hiroです🐼\n何歳からでもやればできる!!\nあきらめないで Keep trying🔥🔥🔥");
- 
-    let newGreeting = document.getElementById("greeting");
-    const indexNo = Math.floor(Math.random() * greeting.length);
-    newGreeting.innerHTML = greeting[indexNo];
-    });
+    aboutMeButton.addEventListener("click", introduceMe);
   }
-});
-
-// お問い合わせボタンがクリックされた時の指示
-let qaButton = document.getElementById("QA"); 
-
-document.addEventListener("DOMContentLoaded", function() {
-  if(qaButton) {
-    qaButton.addEventListener("click", function() {
-    alert("🚧工事中🚧\nただ今 受付けておりません");
-    });
+    function introduceMe() {
+      alert("40代・プログラミング歴１ヶ月の Hiroです🐼\n何歳からでもやればできる!!\nあきらめないで Keep trying🔥🔥🔥");
+   // 画面に表示する文字を変更
+      let newGreeting = document.getElementById("greeting");
+      const indexNo = Math.floor(Math.random() * greeting.length);
+      newGreeting.innerHTML = greeting[indexNo];
+    }
+  
+// お問い合わせボタンがクリックされたらアラート表示
+  if (qaButton) {
+    qaButton.addEventListener("click", respondQa);
   }
-});
+    function respondQa() {
+      alert("🚧工事中🚧\nただ今 受付けておりません");
+    }
