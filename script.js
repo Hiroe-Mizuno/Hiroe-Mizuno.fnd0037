a7450b2230971d52cec2fa6fd6ccb17a998e80ba
Hiroe-Mizuno <hiroe_naruse@mail.toyota.co.jp>
2025-06-01 16:05:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 'use strict'
 
-// ローディング画面を削除する
+// ローディングアニメーション終了後に画面を背面に隠す
 const loadingScreen = document.getElementById("loading");
-setTimeout(deleteLodingScreen, 4900);
-function deleteLodingScreen() {
+setTimeout(hideLodingScreen, 3800);
+function hideLodingScreen() {
   if (loadingScreen) {
-    loadingScreen.remove();
+    loadingScreen.style.visibility = "hidden";
+    loadingScreen.style.zIndex = "-1";
   }
 }
 
@@ -20,7 +21,7 @@ if(aboutMeButton) {
 function introduceMe() {
   alert("40代・プログラミング歴１ヶ月の Hiroです🐼\n何歳からでもやればできる!!\nあきらめないで Keep trying🔥🔥🔥");
   const indexNo = Math.floor(Math.random() * greeting.length);
-  newGreeting.innerHTML = greeting[indexNo];
+  newGreeting.innerText = greeting[indexNo];
 }
 
 // お問い合わせボタンがクリックされたらアラート表示
@@ -30,4 +31,60 @@ if (qaButton) {
 }
 function respondQa() {
   alert("🚧工事中🚧\nただ今 受付けておりません");
+}
+
+// LineUPボタンクリックでローディング画面再生後に背面に隠す
+const lineUpButton = document.getElementById("lineUp"); 
+const loadingText = document.getElementById("loadingText");
+const spinner = document.getElementById("spinner");
+if(lineUpButton) {
+  lineUpButton.addEventListener("click", showLoadingScreen);
+}
+function showLoadingScreen() {
+  // アニメーションリセット
+  loadingScreen.style.animation = "none";
+  loadingText.style.animation = "none";
+
+  void loadingScreen.offsetWidth;
+  void loadingText.offsetWidth;
+  // アニメーションを再設定
+  loadingScreen.style.animation = "fadeout 3s ease-in";
+  loadingText.style.animation = "colChange 3s ease-in";
+  // アニメーションの内容を変更
+  loadingScreen.style.visibility = "visible";
+  loadingText.style.visibility = "visible";
+  spinner.style.visibility = "hidden";
+  loadingScreen.style.opacity = "1";
+  loadingScreen.style.background = "rgba(128, 128, 128, 1)";
+  loadingScreen.style.zIndex = "5";
+  loadingText.innerText = "個性豊かに育ちました！";
+
+  setTimeout(hideLodingScreen, 2800);
+}
+
+// homeボタンクリックでローディング画面再生後に背面に隠す
+const homeButtons = document.querySelectorAll(".home");
+homeButtons.forEach(button => { 
+  button.addEventListener("click", backMainScreen);
+});
+
+function backMainScreen() {
+  // アニメーションリセット
+  loadingScreen.style.animation = "none";
+  spinner.style.animation = "none";
+
+  void loadingScreen.offsetWidth;
+  void spinner.offsetWidth;
+  // アニメーションを再設定
+  loadingScreen.style.animation = "fadeout 2.5s ease-in";
+  spinner.style.animation = "grow 2.5s ease-in";
+  // アニメーションの内容を再設定
+  loadingScreen.style.visibility = "visible";
+  spinner.style.visibility = "visible";
+  loadingText.style.visibility = "hidden";
+  loadingScreen.style.opacity = "1";
+  loadingScreen.style.background = "rgba(128, 128, 128, 1)";
+  loadingScreen.style.zIndex = "5";
+
+  setTimeout(hideLodingScreen, 2300);
 }
